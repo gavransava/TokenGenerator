e33d27c31ae5a8bfaade133145a4cd86965851c3
gavransava <gavransava@gmail.com>
2016-07-16 17:41:48 +0200

    Added new Activities

--- app/src/main/java/com/myexamples/tokengenerator/Tokens.java
+++ app/src/main/java/com/myexamples/tokengenerator/Tokens.java
@@ -2,13 +2,12 @@ package com.myexamples.tokengenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class Tokens extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tokens);
     }
 }
