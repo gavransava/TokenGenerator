985a8555b3500029bf36ed74823d9bcd6d86790b
gavransava <gavransava@gmail.com>
2016-07-16 18:01:45 +0200

    Remove TitleBar + Fullscreen

--- app/src/main/java/com/myexamples/tokengenerator/MainActivity.java
+++ app/src/main/java/com/myexamples/tokengenerator/MainActivity.java
@@ -2,7 +2,6 @@ package com.myexamples.tokengenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
